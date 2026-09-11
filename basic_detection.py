714b9beef5654a38921790565c863bf99440bbcb
Omar Samy <omar3abqarino@gmail.com>
2026-09-12 02:07:18 +0300

    basic detection and cropping using Grounding DINO

--- basic_detection.py
+++ basic_detection.py
@@ -1,10 +1,85 @@
+import os, json, time
+import cv2 as cv
 from autodistill_grounding_dino import GroundingDINO
 from autodistill.detection import CaptionOntology
-import torch
 
 
+# TRAIN_DIR = "Dataset\\train\\images"
+# VAL_DIR = "Dataset\\val\\images"
+# TEST_DIR = "Dataset\\test\\images"
+# INPUT_DIRS = [TRAIN_DIR, VAL_DIR, TEST_DIR]
+INPUT_DIRS = ["test"]
+CROPS_DIR = "D:\\crops"
+METADATA_FILE = "crops_metadata.json"
+PADDING = 7
+
+os.makedirs(CROPS_DIR, exist_ok=True)
 
 
-base_model = GroundingDINO(ontology=CaptionOntology({"cardboard box": "box"}), )
-# box_detector = GroundingDINO(ontology=ontology)
-base_model.label("./test", extension=".jpg")
+box_detector = GroundingDINO(ontology=CaptionOntology({"cardboard box": "box"}))
+
+metadata = {}
+
+print("Scanning images for boxes.")
+
+start = time.time()
+count = 0
+
+
+for INPUT_DIR in INPUT_DIRS:
+    for img_name in os.listdir(INPUT_DIR):
+        if not img_name.lower().endswith(('.jpg', '.png', '.jpeg')): 
+            continue
+            
+        img_path = os.path.join(INPUT_DIR, img_name)
+        image_cv = cv.imread(img_path)
+        
+        if image_cv is None:
+            continue
+            
+        img_h, img_w, _ = image_cv.shape
+        
+        
+        detections = box_detector.predict(img_path)
+        
+        metadata[img_name] = {
+            "img_w": img_w,
+            "img_h": img_h,
+            "boxes": []
+        }
+        
+        
+        for idx, xyxy in enumerate(detections.xyxy):
+            x1, y1, x2, y2 = map(int, xyxy)
+            
+            
+            x1, y1 = max(0, x1 - PADDING), max(0, y1 - PADDING)
+            x2, y2 = min(img_w, x2 + PADDING), min(img_h, y2 + PADDING)
+            
+            crop_cv = image_cv[y1:y2, x1:x2]
+            
+            
+            if crop_cv.size == 0: 
+                continue
+
+            arr = img_name.split(".", -1)[:-1]
+
+            new_name = ".".join(arr)
+            
+            crop_filename = f"{new_name}_crop_{idx}.jpg"
+            crop_path = os.path.join(CROPS_DIR, crop_filename)
+            
+            
+            cv.imwrite(crop_path, crop_cv)
+            
+            
+            metadata[img_name]["boxes"].append({"crop_filename": crop_filename, "x1": x1, "y1": y1, "x2": x2, "y2": y2})
+            count += 1
+    print(f"{INPUT_DIR} is Done")
+    time.sleep(5)
+end = time.time()
+with open(METADATA_FILE, "w") as f:
+    json.dump(metadata, f, indent=4)
+
+print("Done")
+print(f"avg per img: {(end-start)/count} seconds")
